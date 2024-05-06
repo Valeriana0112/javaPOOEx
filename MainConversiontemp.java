d79adba629e5c03c77d402c3d42f13f02fec5e44
valeriana <claudiaae2@gmail.com>
2024-05-05 18:44:57 -0600

    ConversionTemp

---
+++ MainConversiontemp.java
@@ -0,0 +1,12 @@
+
+
+public class MainConversiontemp {
+    public static void main(String[] args) {
+        conversionTemp f = new conversionTemp();
+        f.G = 20;
+        f.mostrarGrados();
+        System.out.println(f.convertir());
+    
+        
+    }
+}
---
+++ conversionTemp.java
@@ -0,0 +1,14 @@
+public class conversionTemp {
+    double G, F;
+    
+
+    public double convertir() {
+       this. F = (G * 9 / 5) + 32;
+        return F;
+    }
+
+    public void mostrarGrados() {
+        System.out.println("Los grados son:" );
+    return ;
+    }
+}
